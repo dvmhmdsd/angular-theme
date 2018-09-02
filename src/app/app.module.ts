4e5cf53385c1c0fcf3781488db72971c49619adf
mohamed-saad <mahameds19999@gmail.com>
2018-09-02 15:33:38 +0200

    initial files

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,25 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { SignupComponent } from './signup/signup.component';
+import { PreviewComponent } from './dashboard/preview/preview.component';
+import { ReportsComponent } from './dashboard/reports/reports.component';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    DashboardComponent,
+    SignupComponent,
+    PreviewComponent,
+    ReportsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
