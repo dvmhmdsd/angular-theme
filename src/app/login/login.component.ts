aba8a5952d2ec3bde0d6a64caa04b0d4333638d7
mohamed-saad <mahameds19999@gmail.com>
2018-09-03 14:28:09 +0200

    build main structure of login

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-login',
@@ -11,5 +11,7 @@ export class LoginComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  
 
 }
---
+++ src/gulpfile.js
@@ -0,0 +1,4 @@
+var gulp = require('gulp'),
+    image = require('gulp-image'),
+    auto = require('autoprefixer'),
+    post = require('gulp-postcss');
