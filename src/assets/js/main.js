f911e17b81b91f97102b865e980e77b7e3f89d8c
mohamed-saad <mahameds19999@gmail.com>
2018-09-10 15:29:53 +0200

    hide the header in login & signup components

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -37,4 +37,8 @@ $(function () {
         $('.not-menu').fadeOut();
     });
 
+    if(window.location.href.indexOf("login") > -1 || window.location.href.indexOf("signup") > -1) {
+        $('header').hide();
+     }
+
 });
